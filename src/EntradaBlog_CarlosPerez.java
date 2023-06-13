e9044deba524519e439811c8f71f9245d2e0ed43
CarlosP <pebeka21@gmail.com>
2023-06-13 13:26:18 +0200

    En segundo lugar utilizaremos el comando /**, */ para los comentarios que tenemos dentro del nuestro código, también hemos cambiado los datos y la ubicación de la información sobre la versión , autor, y fecha  de este proyecto colocandolo arria del todo

--- src/EntradaBlog_CarlosPerez.java
+++ src/EntradaBlog_CarlosPerez.java
@@ -1,22 +1,28 @@
+/**
+ * @autor Carlos Manuel Pérez
+ * @version 2.4
+ */
+
+/**Clase para manejar las entradas de un blog.
+ * La clase se creó el 21/3/2022 por Carlos Pérez.
+ * Esta es la versión 2.3
+ */
+
 public class EntradaBlog_CarlosPerez
 {
 
-    /*Clase para manejar las entradas de un blog.
-     * La clase se creó el 21/3/1999 por Ana López.
-     * Esta es la versión 2.3
-     */
 //Modificar
-        //separador es el ccarácter que separa ENTRADA DE del
-        //nombre del autor
+        /**separador es el ccarácter que separa ENTRADA DE del
+        //nombre del autor*/
 
         public static char separador=':';
         private int id;
         private String texto;
         private String autor;
 
-        //Constructor de la clase. recibe el número de entrada, el nombre del autor
-        //de la entrada y el texto que contiene la entrada. Si el número de entrada
-        //es negativo, lanza una excepción.
+        /**Constructor de la clase. recibe el número de entrada, el nombre del autor
+        *de la entrada y el texto que contiene la entrada. Si el número de entrada
+        es negativo, lanza una excepción.*/
 	public EntradaBlog_CarlosPerez(int id,String autor,String texto)throws IllegalArgumentException{
             if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
             this.id=id;
@@ -31,27 +37,27 @@ public class EntradaBlog_CarlosPerez
             return cad;
         }
 
-        //Devuelve el número de la entrada
+        /**Devuelve el número de la entrada*/
         public int getId(){
             return this.id;
         }
 
-        //devuelve el texto completo de la entrada
+        /**devuelve el texto completo de la entrada*/
         public String getTexto(){
             return this.texto;
         }
 
-        //devuelve el nombre del autor de la entrada en mayúsculas
+        /**devuelve el nombre del autor de la entrada en mayúsculas*/
         public String getAutor(){
             return this.autor.toUpperCase();
         }
 
-        //devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+        /**devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor*/
         public String devuelveAutor(){
             return this.autor;
         }
 
-        //No tiene porqué tener argumentos.
+        /**No tiene porqué tener argumentos.*/
         public static void main(String[] args) {
             //Modificar.
             EntradaBlog_CarlosPerez e1=new EntradaBlog_CarlosPerez (-3,"ana","Últimas noticias, está disponible BixBy 2.0");
